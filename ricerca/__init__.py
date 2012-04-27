3761de0265001db46ef99d8cadf2ccba6c5fb024
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2012-04-27 15:07:25 -0400

    Renamed searcher to ricerca

---
+++ ricerca/__init__.py
@@ -0,0 +1,3 @@
+import content
+
+__all__ = [ "content" ]
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
-setup(name='coconut',
-      version='0.0.0',
-      description='Coconut',
-      author='Ivan E. Cao-Berg',
+setup(name='ricerca',
+      version='1.0.0',
+      description='Content based search based on the algorithm from Wu, Faloutsos, Sycara and Payne. FALCON: Feedback Adaptive Loop for Content-Based Retrieval. Proceeding VLDB 2000 Proceedings of the 26th International Conference on Very Large Data Bases.',
+      author='Ivan Cao-Berg, Baek-Hwan Cho and Jennifer Bakal',
       author_email='icaoberg@cmu.edu',
+      maintainer='Robert F. Murphy',
+      maintainer_email='murphy@cmu.edu',
       url='http://murphylab.web.cmu.edu/software/',
-      py_modules=['coconut'])
-
+      py_modules=['ricerca.content'])
 
