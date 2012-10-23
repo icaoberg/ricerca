ce240cdabfb92c1c06244f1d8dbfdef1b2f3e75a
Ivan E. Cao-Berg <icaoberg@scs.cmu.edu>
2012-10-23 16:14:20 -0400

    Updated author and maintainer lists

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(name='ricerca',
       version='1.0.0',
       description='Content based search based on the algorithm from Wu, Faloutsos, Sycara and Payne. FALCON: Feedback Adaptive Loop for Content-Based Retrieval. Proceeding VLDB 2000 Proceedings of the 26th International Conference on Very Large Data Bases.',
-      author='Ivan Cao-Berg, Baek-Hwan Cho and Jennifer Bakal',
+      author='Ivan Cao-Berg',
       author_email='icaoberg@cmu.edu',
       maintainer='Robert F. Murphy',
       maintainer_email='murphy@cmu.edu',
